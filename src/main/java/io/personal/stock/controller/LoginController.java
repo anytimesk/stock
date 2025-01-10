d79c0fcda238a3e24dabcd7cef5d5d3c4266a420
SKY <sk4u.yoon@gmail.com>
2025-01-10 20:13:50 +0900

    Version 0.0.2  - Bootstrap 테마를 적용  - View단에 thymeleaf를 적용  - 수정한 코드로 Bootstrap 테마 출력 확인

--- src/main/java/io/personal/stock/controller/LoginController.java
+++ src/main/java/io/personal/stock/controller/LoginController.java
@@ -1,12 +1,12 @@
 package io.personal.stock.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LoginController {
     @GetMapping("/")
     public String index() {
-        return "Hello World!";
+        return "index";
     }
 }
