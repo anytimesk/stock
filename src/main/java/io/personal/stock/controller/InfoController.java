8b087a2fc327ddb7662264f1b3a8dcc3ae521df6
SKY <sk4u.yoon@gmail.com>
2025-01-20 21:02:33 +0900

    - 상장회사 정보를 가져오는 기능 구현 - 가져온 정보를 Elastic Search에 저장하는 기능 구현 - 주가조회페이지에서 Elastic Search에 저자오딘 상장 회사 목록 가져와 datalist에 넣는 기능 구현

--- src/main/java/io/personal/stock/controller/InfoController.java
+++ src/main/java/io/personal/stock/controller/InfoController.java
@@ -1,12 +1,10 @@
 package io.personal.stock.controller;
 
 import java.util.HashMap;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,7 +15,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.personal.stock.dto.OpenApiReqParam;
-import io.personal.stock.entity.KRXListedData;
 import io.personal.stock.service.ConfigService;
 import io.personal.stock.service.KRXListedDataService;
 import io.personal.stock.service.OpenApiService;
@@ -25,7 +22,7 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Controller
-public class StockController {
+public class InfoController {
 
     @Autowired
     ConfigService configService;
@@ -36,59 +33,40 @@ public class StockController {
     @Autowired
     KRXListedDataService krxListedDataService;
 
-    @GetMapping(value = "/stock")
-    public String stockMain(Model model) {
+    @GetMapping(value = "/info")
+    public String info() {
 
-        return "stock";
+        return "info";
     }
 
-    @GetMapping(value = "/stock/getCompanyList")
+    @GetMapping(value = "/info/saveCompanyList")
     @ResponseBody
-    public List<KRXListedData> getCompanyList() {
-        List<KRXListedData> data = krxListedDataService.findAll();
-
-        return data;
-    }
-
-    @GetMapping(value = "/stock/getStockPriceInfo")
-    @ResponseBody
-    public ResponseEntity<JsonNode> getStockPriceInfo(@RequestParam String itmsNm, @RequestParam int pageNo,
-            @RequestParam int numOfRows) {
-
-        // DB로부터 data.go.kr 접속을 위한 인증키, URL 정보를 가져온다.
-        HashMap<String, String> data = configService.getConfigData("STOCK_INFO");
+    public ResponseEntity<JsonNode> saveCompanyList(@RequestParam int numOfRows, @RequestParam int pageNo) {
+        HashMap<String, String> data = configService.getConfigData("ISIN_CODE");
         LinkedMultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("serviceKey", data.get("AUTH_KEY"));
-        params.add("numOfRows", Integer.toString(numOfRows));
         params.add("resultType", "json");
-        params.add("itmsNm", openApiService.encodingString(itmsNm, "UTF-8"));
+        params.add("numOfRows", Integer.toString(numOfRows));
+        params.add("pageNo", Integer.toString(pageNo));
 
         OpenApiReqParam reqParam = new OpenApiReqParam();
         reqParam.setEndPointURL(data.get("CALLBACK_URL"));
-        reqParam.setDetailService("/getStockPriceInfo");
+        reqParam.setDetailService("/getItemInfo");
         reqParam.setQueryParam(params);
 
         String response = openApiService.getOpenApiData(reqParam);
-        log.debug("response : {}", response);
-
+        log.info("Get Componey List Response: {}", response);
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode items = null;
 
         try {
             JsonNode jsonNode = objectMapper.readTree(response);
             items = jsonNode.path("response").path("body").path("items").path("item");
-            log.debug("items string : {}", items.toString());
-
-            if (items.isMissingNode()) {
-                log.warn("items 노드를 찾을 수 없습니다.");
-                return ResponseEntity.notFound().build(); // 404 Not Found 응답
-            }
+            krxListedDataService.saveAll(items);
         } catch (JsonProcessingException e) {
             log.error("JSON 파싱 오류: {}", e.getMessage());
-            return ResponseEntity.badRequest().body(null); // 400 Bad Request 응답
         } catch (Exception e) {
             log.error("기타 오류: {}", e.toString());
-            return ResponseEntity.internalServerError().body(null); // 500 Internal Server Error 응답
         }
 
         return ResponseEntity.ok(items);
