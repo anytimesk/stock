884b206ac51d9d386411c20cb38cf96806ef56c7
SKY <sk4u.yoon@gmail.com>
2025-01-28 01:27:45 +0900

    OAuth2 연동 코드 Google login 페이지 구현

--- src/main/java/io/personal/stock/controller/LoginController.java
+++ src/main/java/io/personal/stock/controller/LoginController.java
@@ -1,12 +1,32 @@
 package io.personal.stock.controller;
 
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @Controller
 public class LoginController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(HttpServletRequest request, Model model) {
+
+        log.debug(String.format("Call Log In Page, CsrfToken class getName : %s", CsrfToken.class.getName()));
+
+        CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+        model.addAttribute("csrfToken", csrfToken.getToken());
+        model.addAttribute("csrfParameterName", csrfToken.getParameterName());
+        log.debug(String.format("CSRF Token : %s", csrfToken.getToken().toString()));
+        log.debug(String.format("csrfParameterName : %s", csrfToken.getParameterName()));
+
+        return "login";
     }
 }
---
+++ src/main/java/io/personal/stock/service/impl/OAuth2UserServiceImpl.java
@@ -0,0 +1,20 @@
+package io.personal.stock.service.impl;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OAuth2UserServiceImpl extends DefaultOAuth2UserService {
+
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        return oAuth2User;
+    }
+}
